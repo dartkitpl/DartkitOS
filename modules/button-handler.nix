c1a89fa0e3178872acff075cd324035ed2ae581c
mimovnik <mimovnik@protonmail.com>
2026-03-04 18:51:46 +0100

    feat(button-handler): add deamon that always listens on GPIO

--- modules/button-handler.nix
+++ modules/button-handler.nix
@@ -1,10 +1,34 @@
 {
+  lib,
+  config,
   pkgs,
   ...
 }: let
   # Target is always Raspberry Pi 4 (aarch64-linux)
+  cfg = config.dartkitos.button-handler;
   buttonHandler = pkgs.callPackage ../pkgs/button-handler.nix {};
 in {
-  # Expose the button-handler binary on PATH
-  environment.systemPackages = [buttonHandler];
+  options.dartkitos.button-handler = {
+    enable = lib.mkEnableOption "daemon for handling GPIO button presses";
+  };
+
+  config = lib.mkMerge [
+    {
+      # Expose the button-handler binary on PATH
+      environment.systemPackages = [buttonHandler];
+    }
+    (lib.mkIf cfg.enable {
+      # Create a systemd service to run the button-handler binary
+      systemd.services.button-handler = {
+        description = "Button Handler Service";
+        after = ["network.target"];
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          ExecStart = "${buttonHandler}/bin/button-handler";
+          Restart = "always";
+          User = "root";
+        };
+      };
+    })
+  ];
 }
