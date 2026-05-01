5fcdb34d1a7cbb5eac86f8ba4ee052ef715ef781
mimovnik <mimovnik@protonmail.com>
2026-05-01 11:54:49 +0200

    chore: fully migrate to nixpkgs 25.11

--- modules/flake/nixpkgs.nix
+++ modules/flake/nixpkgs.nix
@@ -4,16 +4,6 @@
       inherit system;
 
       config.allowUnfree = true;
-
-      overlays = [
-        (final: prev: {
-          # Package set from nixpkgs-25-11
-          nixpkgs25 = import inputs.nixpkgs-25-11 {
-            inherit (prev) system;
-            config.allowUnfree = true;
-          };
-        })
-      ];
     };
   };
 }
